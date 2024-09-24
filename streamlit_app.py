18eb53330d0540bf9caacb9b110240fc193eea17
Rabbani N. Kumoro <79595527+rrabbanii@users.noreply.github.com>
2024-09-24 09:43:23 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,7 +62,7 @@ def load_model_classification():
 def load_model_segmentation():
     '''Load the model for segmentation'''
     try:
-        unet_model_id = "1ZWiGHVRI2mfIVcvllvV8SIkmngmTde1n"
+        unet_model_id = "1nRGhEMHL_2wW4Zf5k3DCmEDvwjDc1NPR"
         unet_model_path = "U-Net_Tag_7B.h5"
         if not os.path.exists(unet_model_path):
             try:
